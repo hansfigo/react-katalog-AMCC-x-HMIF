237bf5d17368b9388d8629ede083b5e6bf450973
HansFigo <hambasahaya0303@gmail.com>
2024-07-24 22:43:42 +0700

    starter

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/home';
-import AboutUs from './pages/about-us';
 import Container from './components/Container';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import ProductDetail from './pages/productDetail';
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Container>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about-us" element={<AboutUs />} />
-            <Route path="/products/:id" element={<ProductDetail />}></Route>
-          </Routes>
-        </Container>
-        <Footer />
-      </Router>
+      <Container>
+        {/* Konten Web */}
+      </Container>
     </>
   )
 }
--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="bg-blue-950 text-white text-center p-4 mt-96">
-            <p>&copy; 2024 - CatalogSphere</p>
-        </footer>
-    )
-}
